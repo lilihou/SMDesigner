1116f253b6c9f247a30d66957b23f506705a0afd
auto_struc_mutation_design <42210075+lilihou@users.noreply.github.com>
2023-11-16 11:13:27 -0500

    Update and rename change_file_to_common_struc.py to step3_change_file_to_common_struc.py

--- step3_change_file_to_common_struc.py
+++ step3_change_file_to_common_struc.py
@@ -1,8 +1,8 @@
 import os
 import re
 from pathlib import Path
-data_fold1=Path('/home/lijuanh/projects/def-jonatp/lijuanh/riboswitch_mu_design/seq_struc_riboswitch')
-data_fold2=Path('/home/lijuanh/projects/def-jonatp/lijuanh/riboswitch_mu_design/seq_struc_riboswitch1')
+data_fold1=Path('./seq_struc_riboswitch')
+data_fold2=Path('./seq_struc_riboswitch1')
 finame=os.listdir(data_fold1)
 for i in finame:
     print(i)
@@ -28,29 +28,3 @@ for i in finame:
             fo.write(line)
 
             
-'''
-for i in finame:
-	print(i)
-	fi=open(data_fold1 / i,'r')
-	fo=open(data_fold2 / i,'w')
-	for line in fi.readlines():
-        
-
-		if '#=GC SS_cons' in line:
-			ls=line.split()
-			ls_seq='#=GC SS_cons'
-			line1=re.sub('[\{\[\(]','<',ls[-1])
-			line2=re.sub('[\}\]\)]','>',line1)
-			line3=f'{ls_seq:<50}'+re.sub('[\_\-\:\~]','.',line2)+'\n'
-			fo.write(line3)
-		elif '#=GC cov_SS_cons' in line:
-			fo.write(line)
-		elif '#=GF NUM_COV' in line:
-
-			fo.write(line)
-        else:
-            pass
-        #else:
-           # pass
-'''
-        
