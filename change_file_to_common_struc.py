469a52fad26bd3bd6e222eba10838e5fea60a6eb
auto_struc_mutation_design <42210075+lilihou@users.noreply.github.com>
2023-11-16 11:02:50 -0500

    Add files via upload

---
+++ change_file_to_common_struc.py
@@ -0,0 +1,56 @@
+import os
+import re
+from pathlib import Path
+data_fold1=Path('/home/lijuanh/projects/def-jonatp/lijuanh/riboswitch_mu_design/seq_struc_riboswitch')
+data_fold2=Path('/home/lijuanh/projects/def-jonatp/lijuanh/riboswitch_mu_design/seq_struc_riboswitch1')
+finame=os.listdir(data_fold1)
+for i in finame:
+    print(i)
+    fi=open(data_fold1/i,'r')
+    fo=open(data_fold2/i,'w')
+    for line in fi.readlines():
+        if '#=GC SS_cons' in line:
+            ls=line.split()
+            ls_seq='#=GC SS_cons'
+            line1=re.sub('[\{\[\(]','<',ls[-1])
+            line2=re.sub('[\}\]\)]','>',line1)
+            line3=f'{ls_seq:<45}'+re.sub('[\_\-\:\~]','.',line2)+'\n'
+            fo.write(line3)
+        elif '#=GC cov_SS_cons' in line:
+            ls_id='#=GC cov_SS_cons'
+            line4=f'{ls_id:<45}'+line.strip().split()[-1]+'\n'
+            fo.write(line)
+        elif '#=GF NUM_COV' in line:
+            fo.write(line)
+        elif not line:
+            pass
+        else:
+            fo.write(line)
+
+            
+'''
+for i in finame:
+	print(i)
+	fi=open(data_fold1 / i,'r')
+	fo=open(data_fold2 / i,'w')
+	for line in fi.readlines():
+        
+
+		if '#=GC SS_cons' in line:
+			ls=line.split()
+			ls_seq='#=GC SS_cons'
+			line1=re.sub('[\{\[\(]','<',ls[-1])
+			line2=re.sub('[\}\]\)]','>',line1)
+			line3=f'{ls_seq:<50}'+re.sub('[\_\-\:\~]','.',line2)+'\n'
+			fo.write(line3)
+		elif '#=GC cov_SS_cons' in line:
+			fo.write(line)
+		elif '#=GF NUM_COV' in line:
+
+			fo.write(line)
+        else:
+            pass
+        #else:
+           # pass
+'''
+        
